fcfec5b3fec8e13ecc54c2893636329ee4ded1f9
gtDan7 <kdanta13@gmail.com>
2023-04-26 11:26:39 +0700

    menambahkan paragraf baru

--- index.php
+++ index.php
@@ -9,5 +9,6 @@
 <body>
     <h1>GIT REMOTE</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni quod iusto, quam cupiditate ut voluptas modi temporibus animi nam error pariatur, iure laborum, quasi ipsa maxime! Veniam modi voluptate esse?</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est nobis voluptate natus laboriosam soluta, cumque nam animi porro voluptatum ex quo reprehenderit et quidem fuga, deleniti deserunt necessitatibus earum error.</p>
 </body>
 </html>
